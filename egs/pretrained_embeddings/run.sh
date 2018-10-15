1644e609a2b34642b16ce8a0b27c5caa13310155
Carolina Zheng <carzheng@tesla.ldc.upenn.edu>
2018-10-15 12:06:13 -0400

    Fix run file

--- egs/pretrained_embeddings/run.sh
+++ egs/pretrained_embeddings/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "0. Copying data ... Note that you need to request Newsela data"
-mkdir -p data/newsela_Zhang_Lapata_splits
+echo "0. Copying data and embeddings ... Note that you need to request Newsela data"
+mkdir -p data/newsela_Zhang_Lapata_splits data/emb
 #cp /data1/reno/newsela_Zhang_Lapata_splits/V0V4_V1V4_V2V4_V3V4_V0V3_V0V2_V1V3.aner.ori.* data/newsela_Zhang_Lapata_splits
 cp /data2/text_simplification/dataset/train/* /data2/text_simplification/dataset/valid/* data/newsela_Zhang_Lapata_splits
+cp /data1/embeddings/eng/glove.6B.300d.txt data/emb/glove.6B.300d.txt
 
 hyperparam_file=anon_glove_zhanglapata.hpm
 source $hyperparam_file
@@ -12,8 +13,8 @@ echo "1. Get rootdir from hyperparam file: $rootdir"
 echo "2. Running preprocess-BPE.sh"
 bash $rootdir/scripts/preprocess-bpe.sh $hyperparam_file
 
-echo "3. Starting training"
-bash $rootdir/scripts/train.sh -p $hyperparam_file -e sockeye_gpu
+echo "3. Starting training with pre-trained embeddings"
+bash $rootdir/scripts/train-embeddings.sh -p $hyperparam_file -e sockeye_gpu
 #qsub -S /bin/bash -V -cwd -q gpu.q -l gpu=1,h_rt=240:00:00,num_proc=2 -j y $rootdir/scripts/train.sh -p $hyperparam_file -e sockeye_gpu
 
 # TBD: testset=newstest2014
