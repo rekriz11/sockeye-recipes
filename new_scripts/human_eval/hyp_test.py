812f99288cf17f49a1079da1cf404a244a12e5f4
Carolina Zheng <swirlinq@gmail.com>
2018-12-08 00:17:47 -0500

    Add CIs and mean to hyp tests

--- new_scripts/human_eval/hyp_test.py
+++ new_scripts/human_eval/hyp_test.py
@@ -1,12 +1,14 @@
 import sys
 import csv
 import pandas as pd
-from scipy.stats import ttest_ind
+import scipy.stats as st
 
 # Assume that the last model is the one we're comparing against
 SYSTEMS = ["REFERENCE SIMPLE", "S2S", "HYBRID", \
            "DRESS-LS", "DMASS", "S2S-ALL-FAS", "S2S-ALL-FA"]
 ASPECTS = ['mean', 'simp', 'grammar']
+ALPHA = 0.05
+rd = 5 # number of digits to round to
 
 def read_results(mturk_file):
     mturk_dict = dict()
@@ -23,19 +25,24 @@ def refactor(mturk_dict):
     NUM_PROMPTS = 5
     NUM_SIMPLIFIED = 4
     df_rows = []
-    count = 0
     for _, row in mturk_dict.items():
-        count += 1
         for i in range(1, NUM_PROMPTS + 1):
             for j in range(1, NUM_SIMPLIFIED + 1):
                 model_name = SYSTEMS[int(row[f'Input.id{i}{j}'])]
+                sum_scores = 0
                 for aspect in ASPECTS:
                     score = int(row[f'Answer.{aspect}{i}{j}'])
+                    sum_scores += score
                     df_rows.append({
                         'model': model_name,
                         'aspect': aspect,
                         'score': score
                     })
+                df_rows.append({
+                    'model': model_name,
+                    'aspect': 'avg',
+                    'score': sum_scores / len(ASPECTS)
+                })
     return pd.DataFrame(df_rows)
 
 
@@ -44,9 +51,23 @@ def hyp_test(df):
     for aspect in ASPECTS:
         results[aspect] = []
         s1 = df[(df['aspect'] == aspect) & (df['model'] == SYSTEMS[-1])]
+        deg = 2*len(s1)-2
         for model in SYSTEMS[:len(SYSTEMS)-1]:
             s2 = df[(df['aspect'] == aspect) & (df['model'] == model)]
-            results[aspect].append(ttest_ind(s1['score'], s2['score']))
+            diffs = s1['score'].reset_index(drop=True) - s2['score'].reset_index(drop=True)
+            std = diffs.std() * (2/len(s1))**0.5
+            results[aspect].append((st.ttest_ind(s1['score'], s2['score']),
+                                    st.t.interval(1-ALPHA, deg, loc=diffs.mean(), scale=std)))
+
+    results['avg'] = []
+    s1 = df[(df['aspect'] == 'avg') & (df['model'] == SYSTEMS[-1])]
+    deg = 2*len(s1)-2
+    for model in SYSTEMS[:len(SYSTEMS)-1]:
+        s2 = df[(df['aspect'] == 'avg') & (df['model'] == model)]
+        diffs = s1['score'].reset_index(drop=True) - s2['score'].reset_index(drop=True)
+        std = diffs.std() * (2/len(s1))**0.5
+        results['avg'].append((st.ttest_ind(s1['score'], s2['score']),
+                               st.t.interval(1-ALPHA, deg, loc=diffs.mean(), scale=std)))
     return results
 
 
@@ -59,7 +80,9 @@ def main(mturk_file):
         for aspect, stats in results.items():
             outfile.write(f'===================\nResults for: {aspect}\n===================\n')
             for i, stat in enumerate(stats):
-                outfile.write(f'Versus {SYSTEMS[i]}: stat={round(stat[0], 5)}, p-value={round(stat[1], 5)}\n')
+                outfile.write(f'Versus {SYSTEMS[i]}: stat={round(stat[0][0], rd)}, '
+                              f'p-value={round(stat[0][1], rd)}, '
+                              f'conf=({round(stat[1][0], rd)}, {round(stat[1][1], rd)})\n')
     #df.to_csv('out.csv', index=False)
 
 
